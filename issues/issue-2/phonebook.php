90716d644a36d42911b659c71524b9939fea3ac5
Julien Issler <julien@issler.net>
2017-11-06 15:44:11 +0100

    Issue #2: amélioration du script proxy, pour les Cisco par exemple

--- issues/issue-2/phonebook.php
+++ issues/issue-2/phonebook.php
@@ -2,11 +2,11 @@
 $pathinfo = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
 if(!$pathinfo)
     exit;
-$pathinfo = str_replace('%3F','?', $pathinfo);
+$pathinfo = rawurldecode($pathinfo);
 $xml = file_get_contents('http://pb.voip.ovh.net'.$pathinfo);
 
 $xml = preg_replace_callback('#<URL>http://pb.voip.ovh.net(.+)</URL>#',function($matches){
-    return '<URL>http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'].str_replace('?','%3F',$matches[1]).'</URL>';
+    return '<URL>http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'].str_replace('%2F','/',rawurlencode($matches[1])).'</URL>';
 }, $xml);
 
 $xml = preg_replace_callback('#<Telephone>0033(.*)</Telephone>#',function($matches){
